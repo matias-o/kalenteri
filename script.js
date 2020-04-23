070cc25d2d61dd4e15aa4fb485fa8f12f9dedf4c
SamuliRaivio <64201196+SamuliRaivio@users.noreply.github.com>
2020-04-23 14:41:17 +0300

    asd

---
+++ script.js
@@ -0,0 +1,34 @@
+/*function tarkistaAika(){
+    var date = new Date();
+    var day = date.getDate;
+    var openDate = new Date();
+    openDate.set
+    if (day < )
+    
+} */
+function tarkistaAika() {
+    var viikko = getWeekNumber(new Date());
+    if (viikko > 16){
+        alert("it's a corona time babe!")
+    }
+        
+       
+}
+
+
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return weekNo;
+}
+
+var result = getWeekNumber(new Date());
+document.write(result);
