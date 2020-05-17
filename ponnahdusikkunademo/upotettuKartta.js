7fe417093774e9aaa7c5068eef8ba54d062ca3dd
matias-o <matias.ollanketo@hotmail.com>
2020-05-17 22:39:40 +0300

    Update upotettuKartta.js

--- ponnahdusikkunademo/upotettuKartta.js
+++ ponnahdusikkunademo/upotettuKartta.js
@@ -1,15 +1,3 @@
-function initMap(){
-  var hietsunSali = {lat: 60.172841, lng: 24.903181};
-  var map = new google.maps.Map(
-    document.getElementById('map'), {zoom 4, center: hietsunSali});
-  var marker = new google.maps.Marker({position: hietsunSali, map: map});
-}
-
 function newWindow () {
-  alert("hölhölö");
-}
-
-function karttaAuki() {
-  window.open("js projektit/karttatesti.html", "_blank",
-   "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+  var newWin = open("karttatesti.HTML","windowName","height=300","width=300");
 }
