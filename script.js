4a962184d6145ed4505e04fee8a6189fd5c3e823
SamuliRaivio <64201196+SamuliRaivio@users.noreply.github.com>
2020-05-17 21:15:32 +0300

    asdasdad

--- script.js
+++ script.js
@@ -1,53 +1,80 @@
-/*function tarkistaAika(){
-    var date = new Date();
-    var day = date.getDate;
-    var openDate = new Date();
-    openDate.set
-    if (day < )
-    
-} */
-/*var viikkoNumero = getWeekNumber(new Date());
-var luukku1 = document.getElementById("luukku1").value;
+//TIKO-KESÄKALENTERIN JAVASCRIPT SAMULI RAIVIO, TUOMAS YLÖNEN, MATIAS OLLANKETO
 
-var x = 1;
-while (while) */
+// Funktio antaa tämän hetkisen viikkonumeron
+function viikkonyt(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    return weekNo;
+}
 
+//muuttuja "viikko" on aina tämän hetkisen viikon numero
+var viikko = viikkonyt(new Date());
 
- //document.getElementById("luukku" + 1).innerHTML ="<img src=\"img/musta.jpg\">";
-var viikko = getWeekNumber(new Date());
+//muuttuja "aloitusviikko" on kalenterin aloitusviikon numero
 var aloitusviikko = 20;
-for (var i = 1; i < 14; i++){
+
+//tämä for-loop käy jokaisen kalenteriluukun läpi ja vaihtaa kuvan luukkuihin, joita ei voi avata
+for (var i = 1; i < 14; i++) {
     if (aloitusviikko > viikko) {
-        document.getElementById("luukku" + i).innerHTML ="<img class=\"luukku-img\"  src=\"img/musta.jpg\">";
+        document.getElementById("luukku" + i).innerHTML = "<img class=\"luukku-img\"  src=\"img/musta.jpg\"><br><p>Viikko" + i + "</p>";
     }
     aloitusviikko++;
 }
 
+//JOKAISELLE LUUKULLE ON OMA FUNKTIO
+//jos luukku on "auki" luukun kuvaa voi painaa ja funktio tapahtuu
+//jokaisen luukun avaamisfunktion nimi on "luukunAvaus" + luukun numero
 
-    function tarkistaAika2() {
-        var viikko = getWeekNumber(new Date());
-        if (viikko > 18) {
-            alert("it's a corona time babe!")
-        }
+function luukunAvaus1 () {
+    
+}
 
+function luukunAvaus2 () {
+    
+}
 
-    }
+function luukunAvaus3 () {
+    
+}
 
+function luukunAvaus4 () {
+    
+}
 
+function luukunAvaus5 () {
+    
+}
 
-    function getWeekNumber(d) {
-        // Copy date so don't modify original
-        d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-        // Set to nearest Thursday: current date + 4 - current day number
-        // Make Sunday's day number 7
-        d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-        // Get first day of year
-        var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-        // Calculate full weeks to nearest Thursday
-        var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-        // Return array of year and week number
-        return weekNo;
-    }
+function luukunAvaus6 () {
+    
+}
 
-    /* Viikkonumero */
-    //var result = getWeekNumber(new Date());
+function luukunAvaus7 () {
+    
+}
+
+function luukunAvaus8 () {
+    
+}
+
+function luukunAvaus9 () {
+    
+}
+
+function luukunAvaus10 () {
+    
+}
+
+function luukunAvaus11 () {
+    
+}
+
+function luukunAvaus12 () {
+    
+}
+
+function luukunAvaus13 () {
+    
+}
